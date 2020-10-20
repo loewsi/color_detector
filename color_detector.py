1706cd73a94deb4c5acfc63ef2a3b7ca6b4bff74
silvan <loewsi@ethz.ch>
2020-10-20 09:41:31 +0200

    clustering complete image, searching for main color in splits

--- color_detector.py
+++ color_detector.py
@@ -11,7 +11,7 @@ cap = cv2.VideoCapture(2)
 
 n_splits = int(os.environ['N_SPLITS'])
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 2
+K = 10
 top_colors = np.zeros((n_splits, 3))
 
 
@@ -23,24 +23,21 @@ while(True):
     # You can now treat output as a normal numpy array
     # Do your magic here
     Z = frame.reshape((-1, 3))
-    Z_length = np.size(Z)
-    split_length = Z_length/n_splits
     Z_float = np.float32(Z)
-    print()
-    for i in range(0, (n_splits-1)):
+    ret, label, center = cv2.kmeans(Z_float, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    center_int = np.uint8(center)
+    res = center_int[label.flatten()]
+    split_length = np.size(label)/n_splits
+
+    for i in range(0, n_splits):
         begin = int(math.floor(i*split_length))
-        print(begin)
         end = int(math.floor((i+1)*split_length))
-        print(end)
-        Z_split = Z_float[begin:end]
-        print(Z_split)
-        ret, label, center = cv2.kmeans(Z_split, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-        unique, counts = np.unique(label, return_counts=True)
+        label_split = label[begin:end]
+        unique, counts = np.unique(label_split, return_counts=True)
         ind = np.argmax(counts)
-        top_color = center[ind, :]
+        top_color = center_int[ind, :]
         top_colors[i, :] = top_color
-        print(i)
 
     print(top_colors)
 
-    sleep(3)
+    sleep(1)
